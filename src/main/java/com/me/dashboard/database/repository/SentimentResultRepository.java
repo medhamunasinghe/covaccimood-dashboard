5793c54c76a8c39c21d87dc90c363d5052431164
Medha Munasinghe <medhamunasinghe2000@gmail.com>
2023-04-17 15:59:49 +0530

    Method changes

--- src/main/java/com/me/dashboard/database/repository/SentimentResultRepository.java
+++ src/main/java/com/me/dashboard/database/repository/SentimentResultRepository.java
@@ -1,12 +1,9 @@
 package com.me.dashboard.database.repository;
 
 import com.me.dashboard.database.entities.SentimentResult;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
 
-@Repository
+
 public interface SentimentResultRepository extends CrudRepository<SentimentResult, Long> {
 
     // @Query(value = "SELECT COUNT(id) FROM covaccimood-db.sentence where sentiment ='Positive'", nativeQuery = true)
